759937cd23c05a63b684eaa44d7a2ae58bbaf61c
NikolajMollerup <NikolajMollerup@gmail.com>
2021-09-05 17:52:27 +0200

    worked on the style

--- droemmehavet_homepage/src/Frontpage/Frontpage_Components/Frontpage.js
+++ droemmehavet_homepage/src/Frontpage/Frontpage_Components/Frontpage.js
@@ -4,14 +4,14 @@ import backgroundImg from "../../background.png";
 export default function Frontpage() {
   return (
     <div className="frontpage">
-      <h1>Velkommen til Drømmehavet</h1>
-      <h2>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Dignissim diam quis
-        enim lobortis scelerisque fermentum dui faucibus. Lacus viverra vitae
-        congue eu consequat ac.
-      </h2>
-      <button>Prøv 3 måneder!</button>
+      <div id="contentContainer">
+        <h1>Velkommen til Drømmehavet</h1>
+        <h2>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </h2>
+        <button className="FreeTrailButton">Prøv 3 måneder!</button>
+      </div>
     </div>
   );
 }
--- droemmehavet_homepage/src/Shared/Header.js
+++ droemmehavet_homepage/src/Shared/Header.js
@@ -4,11 +4,11 @@ export default function Header() {
   return (
     <div className="header">
       <h1>Drømmehavet</h1>
-      <div className="headerButtons">
-        <button>webshop</button>
-        <button>om os</button>
-        <button>kontakt</button>
-        <button>log in</button>
+      <div className="headerButtonsContainer">
+        <button className="headerButton">webshop</button>
+        <button className="headerButton">om os</button>
+        <button className="headerButton">kontakt</button>
+        <button id="logInButton">log in</button>
       </div>
     </div>
   );
