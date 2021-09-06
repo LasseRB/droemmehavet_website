2fa104659a63d99a8a95924ad3d2815cccc99bb1
NikolajMollerup <NikolajMollerup@gmail.com>
2021-09-06 22:46:32 +0200

    Worked on lower part of frontpage

--- droemmehavet_homepage/src/Frontpage/Frontpage_Components/Frontpage.js
+++ droemmehavet_homepage/src/Frontpage/Frontpage_Components/Frontpage.js
@@ -12,6 +12,18 @@ export default function Frontpage() {
         </h2>
         <button className="FreeTrailButton">Prøv 3 måneder!</button>
       </div>
+      <div id="secondContentContainer">
+        <div id="tutorielText"> 
+        <h1>At vero </h1>
+        <h2>eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</h2>
+        </div>
+        <figure id="imgContainer">
+          <img
+            id="exampleImg"
+            src="https://firebasestorage.googleapis.com/v0/b/droemmehavet-1aa53.appspot.com/o/islands%2Feventyr%C3%B8en%2FBG-lowres.jpg?alt=media&"
+          />
+        </figure>
+      </div>
     </div>
   );
 }
