933f3bfbcd834d3a2942be8a10b008f19ecc352c
LasseRB <lassebrunt@gmail.com>
2021-09-29 19:14:26 +0200

    window scrollTo Tooop

--- droemmehavet_homepage/src/HandelsBetingelserPage/HandelsBetingelser_Components/HandelsBetingelser.js
+++ droemmehavet_homepage/src/HandelsBetingelserPage/HandelsBetingelser_Components/HandelsBetingelser.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 
 export default function HandelsBetingelser() {
+    useEffect(() => {
+        window.scrollTo(0,0);
+    }, [])
     return (
         <div className="tekstbox">
             <h1>Handelsbetingelser for Drømmehavet ApS</h1>
