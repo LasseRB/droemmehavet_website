60c7b1c57cb258185d98cac819bd237109f15fd3
LasseRB <lassebrunt@gmail.com>
2021-10-20 22:23:54 +0200

    cors

--- src/Shared/Reepay/reepay.js
+++ src/Shared/Reepay/reepay.js
@@ -4,6 +4,7 @@ class Reepay{
         return await fetch('https://droem-hn4sec6b7a-lz.a.run.app/reepay/subsession?handle='+handle,
             {
                 method: 'GET',
+                mode: 'cors',
                
             }).then(res => res.json())
             .catch(error => console.error(error))
@@ -15,6 +16,7 @@ class Reepay{
         return await fetch('https://droem-hn4sec6b7a-lz.a.run.app/reepay/pendingsub?handle='+handle+'&fornavn='+fornavn+'&efternavn='+efternavn+'&email='+email,
             {
                 method: 'GET',
+                mode: 'cors',
                 
             }).then(response => response.json())
             .catch(error => console.error(error))
@@ -30,14 +32,11 @@ class Reepay{
     }
 
     async getCustomer(handle) {
-        return await fetch('https://api.reepay.com/v1/customer/'+handle,
+        return await fetch('https://droem-hn4sec6b7a-lz.a.run.app/reepay/customer?handle='+handle,
         {
             method: 'GET',
-            headers: {
-                'authorization': 'Basic ' + this.getAPIKeyAsBase64(),
-                'content-type': 'application/json',
-                'accept': 'application/json'
-             }
+            mode: 'cors'
+          
         }).then(response => response.json())
         .catch(error => error.json())
     
@@ -45,7 +44,7 @@ class Reepay{
 
     createNewSubscriptionHandle = (userID) =>{
         if(userID != null && userID.length > 0)
-            return 'subscription-'+userID
+            return 'subscription-'+(Math.random()* 10) +userID
         else
             return 'subscription-'+ Math.random() * 10
     }
