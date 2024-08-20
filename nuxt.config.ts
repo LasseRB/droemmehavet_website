84b56db25cb2d087d22f668ac5d447ac4483e4c7
LasseRB <lassebrunt@gmail.com>
2024-08-20 21:21:27 +0200

    nuxt congif

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,13 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
+  nitro: {
+    firebase: {
+      gen: 2,
+    }
+  },
+  routeRules: {
+    '/': { ssr: false },
+    '/blog': { ssr: true }
+  }
 })
