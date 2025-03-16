3d9d4c2e6796a380114e2ab7d29c073407022641
LasseRB <lassebrunt@gmail.com>
2025-03-16 12:04:32 +0100

    cors - allow * origins (test)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,8 @@ export default defineNuxtConfig({
   modules: ['nuxt-security'],
   security: {
     corsHandler: {
-      origin: ['https://droemmehavet.dk/','https://blog.droemmehavet.dk/wp-json/wp/v2/media', 'https://blog.droemmehavet.dk/wp-json/wp/v2/users', 'https://blog.droemmehavet.dk/wp-json/wp/v2/posts/?_fields=author,id,date,title,link,content,featured_media,excerpt'],
+      // origin: ['https://droemmehavet.dk/','https://blog.droemmehavet.dk/wp-json/wp/v2/media', 'https://blog.droemmehavet.dk/wp-json/wp/v2/users', 'https://blog.droemmehavet.dk/wp-json/wp/v2/posts/?_fields=author,id,date,title,link,content,featured_media,excerpt'],
+      origin: '*',
       useRegExp: true,
       allowHeaders: '*'
     },
